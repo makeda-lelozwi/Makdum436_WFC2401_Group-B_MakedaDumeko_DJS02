6dea020ab45249d9d5b38be74176c41b926c5dd2
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-01 18:56:12 +0200

    resolved the division resulting in decimals story

--- scripts.js
+++ scripts.js
@@ -5,5 +5,15 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  //Whole division
   result.innerText = dividend / divider;
+
+  //Division that results in a decimal
+  let answer = dividend / divider;
+
+  if (dividend % divider !== 0) {
+    result.innerText = Math.floor(answer);
+  }
+
+  
 });
