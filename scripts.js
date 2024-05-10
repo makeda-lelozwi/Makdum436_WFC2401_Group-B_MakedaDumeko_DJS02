a7ec422c3f690b1ecc3cc49e4949cdc0992e0001
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-10 12:38:01 +0200

    refactored solution to include try/catch

--- scripts.js
+++ scripts.js
@@ -5,30 +5,34 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  //Whole division
-  result.innerText = dividend / divider;
 
-  //Division that results in a decimal
-  let answer = dividend / divider;
-
-  if (dividend % divider !== 0) {
-    result.innerText = Math.floor(answer);
-  }
-
-  //No values, division by 0, inputting non-numbers
+  //the input fields in HTML don't have classes or IDs
   const dividendEl = document.querySelector("input[name='dividend']");
   const dividerEl = document.querySelector("input[name='divider']");
 
-  if (dividendEl.value == "" || dividerEl.value == "") {
-    result.innerText =
-      "Division not performed. Both values are required in inputs. Try again.";
-  } else if (dividerEl.value == 0) {
-    result.innerText =
-      "Division not performed. Invalid number provided. Try again.";
-  } else if (isNaN(dividendEl.value) || isNaN(dividerEl.value)) {
-    document.body.innerText =
-      "Something critical went wrong. Please reload the page";
+  let answer = Math.floor(dividend / divider);
+
+  try {
+    if (dividend % divider !== 0) {
+      result.innerText = answer;
+    }
+    if (dividendEl.value == "" || dividerEl.value == "") {
+      throw new Error(
+        "Division not performed. Both values are required in inputs. Try again."
+      );
+    } else if (dividerEl.value == 0) {
+      throw new Error(
+        "Division not performed. Invalid number provided. Try again."
+      );
+    } else if (isNaN(dividendEl.value) || isNaN(dividerEl.value)) {
+      throw new Error(
+        (document.body.innerText =
+          "Something critical went wrong. Please reload the page")
+      );
+    } else result.innerText = answer;
+  } catch (error) {
+    result.innerText = error.message;
   }
 
-  //
+  console.log(result.innerText);
 });
